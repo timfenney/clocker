7f0f9602ebdc3bf744dbf5e0f3a8f3ca0ccd1a84
Timothy Fenney <kodekata@gmail.com>
2019-03-06 15:18:51 -0500

    Update db seeds for more autocomplete results.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-people = Person.create([
-                         { name: 'Emily', type: 'teacher' },
-                         { name: 'Bob', type: 'teacher' },
-                         { name: 'Mary', type: 'teacher' }                         
-                       ])
+require 'json'
+jsonblob = <<-LISTEND
+["Azral","Azarria","Azriel","Byrne","Bywoods","Bysshe","Czarra","Cyrus","Cyrillus","Dzoba","Dymphia","Dysart","Eziechiele","Ezzo","Fusco","Furr","Gwynne","Gytle","Gyatt","Hyps","Hyozo","Hynes","Hyrup","Izak","Izabel","Izy","Ivonne","Juta","Jutta","Jyoti","Justinian","Justis","Kyrstin","Kynthia","Kyte","Lysander","Lyudmila","Lyontine","Myrwyn","Myrvyn","Nyhagen","Nyssa","Nysa","O'Toole","O'Neill","O'Reilly","O'Shee","Pytlik","Pyotr","Purse","Quitt","Quiteri","Qulllon","Rysler","Ryter","Ryun","Symons","Syst","Symer","Sylvester","Syverson","Tyson","Tyree","Tyre","Uzzial","Uzzia","Utter","Uzzi","Vories","Vyse","Vyky","Vyner","Wyon","Wynn","Wynny","Xymenes","Xenos","Ximenez","Xylon","Yvette","Yuu","Yvonner","Zuzana","Zwiebel"]
+LISTEND
+
+first_names = JSON.parse(jsonblob)
+
+people = []
+first_names.each do |name|
+  person = Person.create({ name: name, type: 'teacher' })
+  people << person
+end
 
 EVENT_TYPES = ['clock-in', 'clock-out']
 people.each do |person|
-  (0..10).each do |i|
+  times = rand(20)
+  (0..times).each do |i|
     index = i % 2
     Event.create(person: person, type: EVENT_TYPES[index])
   end
