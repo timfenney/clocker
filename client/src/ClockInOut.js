4a77771590634a65d0f89a7c14718838051c8cdf
Timothy Fenney <kodekata@gmail.com>
2019-03-06 15:18:19 -0500

    Update icon on main page.

--- client/src/ClockInOut.js
+++ client/src/ClockInOut.js
@@ -8,7 +8,8 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Input from '@material-ui/core/Input';
 import InputLabel from '@material-ui/core/InputLabel';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+//import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import AccessTimeOutlinedIcon from '@material-ui/icons/AccessTimeOutlined'; //OutlinedIcon';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import withStyles from '@material-ui/core/styles/withStyles';
@@ -108,7 +109,7 @@ class ClockInOut extends React.Component {
         <CssBaseline />
         <Paper className={classes.paper}>
             <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
+            <AccessTimeOutlinedIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
             Clock In / Out
